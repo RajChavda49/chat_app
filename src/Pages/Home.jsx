0b099438cbe98fb51841a5de7961b3348106b217
Raj Chavda <raj.theappideas@gmail.com>
2024-01-12 01:09:28 +0530

    add toml:

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const { user } = useSelector((s) => s.auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user === null) {
+      navigate("/auth");
+    }
+  }, []);
+  return <div>Home</div>;
+};
 
-export default Home
+export default Home;
